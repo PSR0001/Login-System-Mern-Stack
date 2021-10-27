50b33c402a383bc129a8622463c14557a9a311d7
Partha <parthasingharoy166@gmail.com>
2021-10-27 17:28:56 +0530

    adding svg file in mern stack for bakend

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,27 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return (
-            <div>
-                <p  className="p-5" >Welcome</p>
-                <div>Welcome to Kgec</div>
-            </div>
+            <>
+         
+                <section className="cont">
+                     <div className="content">
+                         <h1> Learn Web Design, <br></br>
+                             Coding & Much More
+                         </h1>  
+                         <p> Get your free trial started today! </p>
+                      </div>
+                 <div className="form">
+                  <p id="title"> Get Started Today! </p>
+                        <form>
+                    <input type="text" placeholder="Name" />
+                  <input type="text" placeholder="Email" /> <br></br>
+                    <input type="email" placeholder="Message" /> <br></br>
+                      <input type="submit" />
+                    </form>
+                    </div>
+                   </section>
+                  
+                   </>
         );
     }
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,10 +3,26 @@ import React, { Component } from 'react';
 class signup extends Component {
     render() {
         return (
-            <div>
-                <p>Welcome</p>
-                <div>Welcome to Kgec Signup page</div>
-            </div>
+            <>
+                <div className='bold-line'></div>
+                <div className='container'>
+                  <div className='window'>
+                   <div className='overlay'></div>
+                    <div className='content'>
+                     <div className='welcome'>Hello There!</div>
+                     <div className='subtitle'>We're almost done. Before using our services you need to create an account.</div>
+                     <div className='input-fields'>
+                       <input type='text' placeholder='Username' className='input-line full-width'></input>
+                       <input type='email' placeholder='Email' className='input-line full-width'></input>
+                        <input type='password' placeholder='Password' className='input-line full-width'></input>
+
+                     </div>
+                     <div className='spacing'>or continue with <span className='highlight'>Facebook</span></div>
+                    <div><button className='ghost-round full-width'>Create Account</button></div>
+                    </div>
+                 </div>
+                </div>
+            </>
         );
     }
 }
