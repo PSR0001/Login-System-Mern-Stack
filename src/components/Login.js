0b64006b07dca7b6600e24cbb2e206154df83cf4
PSR0001 <83899035+PSR0001@users.noreply.github.com>
2021-12-13 09:07:41 +0530

    connected to fetch function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,28 @@
 import React, { Component } from 'react';
 
 class login extends Component {
-    render() {
-        return (
-            <>
-                <div className="login-wrap">
-	<div className="login-html">
-		<input id="tab-1" type="radio" name="tab" className="sign-in" checked /><label htmlFor="tab-1" className="tab">Sign In</label>
-		<input id="tab-2" type="radio" name="tab" className="sign-up" /><label htmlFor="tab-2" className="tab">Sign Up</label>
-		<div className="login-form">
-			<div className="sign-in-htm">
-				<div class="group">
-					<label for="user" className="label">Email Id</label>
-					<input id="user-1" type="text" className="input" />
+	render() {
+		return (
+
+			<>
+			  <div className="container">
+				<div className="mb-3 my-3 row">
+					<label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
+					<div className="col-sm-10">
+						<input type="text" readOnly className="form-control-plaintext" id="staticEmail" placeholder="email@example.com"/>
+					</div>
 				</div>
-				<div class="group">
-					<label for="pass" className="label">Password</label>
-					<input id="pass-1" type="password" className="input" data-type="password" />
+				<div className="mb-3 my-3 row">
+					<label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+					<div className="col-sm-10">
+						<input type="password" className="form-control" id="inputPassword2"/>
+					</div>
 				</div>
-				<div className="group">
-					<input id="check" type="checkbox" className="check" />
-					<label for="check"><span className="icon"></span> Keep me Signed in</label>
+				<button className="btn btn-primary my3">Submit</button>
 				</div>
-				<div className="group">
-					<input type="submit" className="button" value="Sign In" />
-				</div>
-				<div className="hr"></div>
-				<div className="foot-lnk">
-					<a href="#forgot">Forgot Password?</a>
-				</div>
-			</div>
-			<div className="sign-up-htm">
-				<div className="group">
-					<label for="user" className="label">Username</label>
-					<input id="user-2" type="text" className="input" />
-				</div>
-				<div className="group">
-					<label for="pass" className="label">Password</label>
-					<input id="pass-2" type="password" className="input" data-type="password" />
-				</div>
-				<div class="group">
-					<label for="pass" class="label">Repeat Password</label>
-					<input id="pass-3" type="password" class="input" data-type="password" />
-				</div>
-				<div class="group">
-					<label for="pass" class="label">Email Address</label>
-					<input id="pass-4" type="text" class="input" />
-				</div>
-				<div className="group">
-					<input type="submit" class="button" value="Sign Up"/>
-				</div>
-				<div className="hr"></div>
-				<div className="foot-lnk">
-					<label for="tab-1" />Already Member?
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-            </>
-        );
-    }
+			</>
+		);
+	}
 }
 
 export default login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,30 +1,79 @@
-import React, { Component } from 'react';
 
-class signup extends Component {
-    render() {
-        return (
-            <>
-                <div className='bold-line'></div>
-                <div className='container'>
-                  <div className='window'>
-                   <div className='overlay'></div>
-                    <div className='content'>
-                     <div className='welcome'>Hello There!</div>
-                     <div className='subtitle'>We're almost done. Before using our services you need to create an account.</div>
-                     <div className='input-fields'>
-                       <input type='text' placeholder='Username' className='input-line full-width'></input>
-                       <input type='email' placeholder='Email' className='input-line full-width'></input>
-                        <input type='password' placeholder='Password' className='input-line full-width'></input>
+import React, { useState } from 'react'
+import { useHistory} from 'react-router-dom'
 
-                     </div>
-                     <div className='spacing'>or continue with <span className='highlight'>Facebook</span></div>
-                    <div><button className='ghost-round full-width'>Create Account</button></div>
-                    </div>
-                 </div>
-                </div>
-            </>
-        );
+export default function Signup() {
+    const history = useHistory();
+    const [users, setUsers] = useState({
+        name: "", email: "", phone: "", work: "", password: "", cpassword: ""
+    })
+    let name, value;
+    const handelUserData = (e) => {
+        // console.log(e)
+        name = e.target.name
+        value = e.target.value
+        setUsers({ ...users, [name]: value })
+     
     }
-}
+    //post  data
+    const postUserData = async (e)=>{
+        e.preventDefault()
 
-export default signup;
+        let { name, email, phone, work, password, cpassword } = users;
+        const response = await fetch("/register",{
+            method: "POST",
+            headers:{
+            "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name, email, phone, work, password, cpassword
+            })
+        })
+        const data = await response.json()
+
+        if (data.status === 422 || !data) {
+            window.alert('Invail Registration')
+        }
+        else{
+            window.alert("Registration Successfill")
+            history.push("/login")
+        }
+    }
+
+
+    return (
+        <>
+            <div className="container">
+                <div className="mb-3 my-3">
+                    <label htmlFor="Name" className="form-label">Name</label>
+                    <input type="text" name="name" className="form-control" id="userName" value={users.name} onChange={handelUserData} placeholder="Enter Your Name Here" />
+                </div>
+                <div className="mb-3 my-3">
+                    <label htmlFor="Email" className="form-label">Email address</label>
+                    <input type="email" name="email" className="form-control" id="userEmail" value={users.email} onChange={handelUserData} placeholder="Enter Your Email " />
+                </div>
+                <div className="mb-3 my-3">
+                    <label htmlFor="Phone" className="form-label">Phone</label>
+                    <input type="text" name="phone" className="form-control" id="userPhone" value={users.phone} onChange={handelUserData} placeholder="Enter Your Phone Number " />
+                </div>
+                <div className="mb-3 my-3">
+                    <label htmlFor="Work" className="form-label">Work</label>
+                    <input type="text" name="work" className="form-control" id="userWork" value={users.work} onChange={handelUserData} placeholder="Enter Your Work Hare" />
+                </div>
+                <div className="mb-3 row">
+                    <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                        <input type="password" name="password" className="form-control" id="inputPassword1" value={users.password} onChange={handelUserData} />
+                    </div>
+                    <div className="mb-3 row">
+                        <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Confirm Password</label>
+                        <div className="col-sm-10">
+                            <input type="password" name="cpassword" className="form-control" id="inputPassword" value={users.cpassword} onChange={handelUserData}/>
+                        </div>
+                        <button onClick={postUserData}  className="btn btn-primary my-3">Submit</button>
+                 </div>
+               </div>
+               </div>
+                </>
+ )
+}
