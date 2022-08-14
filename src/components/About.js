5a56e4d1bd6b3bcc45c9eca571077a0b85d5c152
CoolD69 <parthasingharoy166@gmail.com>
2022-08-14 20:09:51 +0530

    all ok

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,41 @@
-import React,{useEffect} from 'react'
-import {useHistory} from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+// import Carosole from './Carosole';
 import Navbar from './Navbar';
+import Cv from './Cv';
+import { FaFacebookF } from 'react-icons/fa';
+import { AiOutlineGoogle, AiFillGithub } from 'react-icons/ai';
+import { ImLinkedin2 } from 'react-icons/im';
+
+import img from "../img/171.jpg"
+
 
 const About = () => {
     const history = useHistory();
 
-    const callAboutPage = async ()=>{
-        try{
+    const callAboutPage = async () => {
+        try {
             const response = await fetch('/about', {
                 method: "POST",
-                mode:"cors",
+                mode: "cors",
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json"
                 },
-                credentials:"include",
-                
+                credentials: "include",
+
             })
-           
+
             const data = await response.json();
             console.log(data.status)
-            if (data.status === 401 || !data) {
-                window.alert('Please Log In!')
-                history.push("/login")
-            }
+            // if (data.status === 401 || !data) {
+            //     window.alert('Please Log In!')
+            //     history.push("/login")
+            // }
 
         }
-        catch(err){
-            console.log('i am error'+err)
+        catch (err) {
+            console.log('i am error' + err)
             history.push("/login")
         }
     }
@@ -38,14 +46,52 @@ const About = () => {
         callAboutPage();
         console.log('hi i am useeffect hook')
     })
-        
-  
+
+
 
     return (
         <div>
             <Navbar />
-            <p>Welcome</p>
-            <div>Welcome to Kgec About page</div>
+            {/* <Carosole /> */}
+            <div className="" >
+                <img src={img} className="w-100" alt="120" />
+            </div>
+
+            <div className="text-center container ">
+                <div className="card mx-4 mx-md-5 container1245 shadow-" >
+                    <div className="card-body py-5 px-md-5 ">
+                        <div className="row d-flex justify-content-center ">
+                            <div className="col-lg-8">
+                                <h2 className="fw-bold mb-5">Hi! I am Partha👋...</h2>
+
+
+                                <Cv />
+
+                                <div className="text-center">
+                                    <p>or Follow me :</p>
+                                    <button type="button" className="btn btn-link btn-floating mx-1">
+                                        <FaFacebookF />
+                                    </button>
+
+                                    <button type="button" className="btn btn-link btn-floating mx-1">
+                                        <AiOutlineGoogle />
+                                    </button>
+
+                                    <button type="button" className="btn btn-link btn-floating mx-1">
+                                        <ImLinkedin2 />
+                                    </button>
+
+                                    <button type="button" className="btn btn-link btn-floating mx-1">
+                                        <AiFillGithub />
+                                    </button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,58 +1,98 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar';
+import React from 'react'
+import Navbar from './Navbar'
+import img from '../img/partha.jpg'
+import { FaFacebookF } from 'react-icons/fa';
+import { AiOutlineGoogle, AiFillGithub } from 'react-icons/ai';
+import { ImLinkedin2 } from 'react-icons/im';
 
-class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                
-
-
-                <div className="container py-4">
-
-<form id="contactForm" data-sb-form-api-token="API_TOKEN">
-
-  <div className="mb-3">
-    <label className="form-label" for="name">Name</label>
-    <input className="form-control" id="name" type="text" placeholder="Name" data-sb-validations="required" />
-    <div className="invalid-feedback" data-sb-feedback="name:required">Name is required.</div>
-  </div>
-
-  <div className="mb-3">
-    <label className="form-label" for="emailAddress">Email Address</label>
-    <input className="form-control" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required, email" />
-    <div className="invalid-feedback" data-sb-feedback="emailAddress:required">Email Address is required.</div>
-    <div className="invalid-feedback" data-sb-feedback="emailAddress:email">Email Address Email is not valid.</div>
-  </div>
-
-  <div className="mb-3">
-    <label className="form-label" for="message">Message</label>
-    <textarea className="form-control" id="message" type="text" placeholder="Message"  data-sb-validations="required"></textarea>
-    <div className="invalid-feedback" data-sb-feedback="message:required">Message is required.</div>
-  </div>
-
-  <div className="d-none" id="submitSuccessMessage">
-    <div className="text-center mb-3">Form submission successful!</div>
-  </div>
-
-  <div className="d-none" id="submitErrorMessage">
-    <div className="text-center text-danger mb-3">Error sending message!</div>
-  </div>
-
-  <div className="d-grid">
-    <button className="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button>
-  </div>
-
-</form>
-
-</div>
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
 
 
+
+      <section className="text-center text-lg-start container">
+
+        <div className="container py-4 ">
+          <div className="row g-0 align-items-center visibility2">
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <div className="card cascading-right op shadow-lg" >
+                <div className="card-body p-5 shadow-5 text-center">
+                  <h2 className="fw-bold mb-5">Contact Me</h2>
+                  <form>
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <input type="text" id="form3Example1" className="form-control" />
+                          <label className="form-label" htmlFor="form3Example1">First name</label>
+                        </div>
+                      </div>
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <input type="text" id="form3Example2" className="form-control" />
+                          <label className="form-label" htmlFor="form3Example2">Last name</label>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <input type="email" id="form3Example3" className="form-control" />
+                      <label className="form-label" htmlFor="form3Example3">Email address</label>
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <textarea type="text" id="form3Example4" className="form-control" />
+                      <label className="form-label" htmlFor="form3Example4">Message</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block mb-4">
+                      Message
+                    </button>
+
+                    <div className="text-center">
+                      <p>or Follow me :</p>
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <FaFacebookF />
+                      </button>
+
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <AiOutlineGoogle />
+                      </button>
+
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <ImLinkedin2 />
+                      </button>
+
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <AiFillGithub />
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-5 mb-lg-0 visibility1 container123">
+              <img src={img} className="w-100 image"
+                alt="img" />
+              <div className="overlay">
+                <div className="text">
+               <p> Hello, I am Partha Singha Roy. I have rich interest in Web Development, Electronics, IoT. Born in Nadia, West Bengal. It has been a long journey till here. I hope this website will interest you in my life.</p>
+                </div>
+
+              </div>
 
             </div>
-        );
-    }
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+    </div>
+  )
 }
 
-export default Contact;
+export default Contact
