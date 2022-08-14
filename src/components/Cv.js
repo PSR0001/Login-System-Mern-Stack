354278fc1f3fb47b87a0ccfe5a6281ad928417c3
CoolD69 <parthasingharoy166@gmail.com>
2022-08-14 20:10:06 +0530

    add cv

---
+++ src/components/Cv.js
@@ -0,0 +1,205 @@
+import React from 'react'
+
+const Cv = () => {
+  return (
+    <div>
+        <p>Partha Singha Roy</p>
+<p>I am a <strong>Web Developer</strong>, <strong>IoT Enthusiast</strong> and <strong>Electronics Hobbyist</strong>.</p>
+<p>&nbsp;</p>
+<p><strong><u>Education</u></strong><strong>:</strong></p>
+<p>B.Tech, Kalyani Government Engineering College, ECE 2020-2024.</p>
+<p><strong><u>TECHNICAL SKILLS:</u></strong></p>
+<p>The Things I am good enough <strong><u>&nbsp;&nbsp;&nbsp;&nbsp;</u></strong></p>
+<ul>
+<li><strong><u>Programming</u></strong><strong> :<u> </u></strong></li>
+</ul>
+<table width="0">
+<tbody>
+<tr>
+<td width="191">
+<p>C/C++</p>
+<p>Advanced</p>
+<p>&nbsp;</p>
+</td>
+<td width="210">
+<p>JavaScript</p>
+<p>Advanced</p>
+</td>
+<td width="236">
+<p>React JS</p>
+<p>Intermediate</p>
+</td>
+</tr>
+<tr>
+<td width="191">
+<p>Node JS</p>
+<p>Intermediate</p>
+<p>&nbsp;</p>
+</td>
+<td width="210">
+<p>MERN STACK&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+<p>Intermediate</p>
+</td>
+<td width="236">
+<p>Next JS</p>
+<p>Advanced</p>
+</td>
+</tr>
+</tbody>
+</table>
+<ul>
+<li><strong><u>Hardware knowledge</u></strong> <strong>:</strong></li>
+</ul>
+<table>
+<tbody>
+<tr>
+<td width="143">
+<p>Arduino Framework</p>
+</td>
+<td width="133">
+<p>ESP8266</p>
+<p>&nbsp;</p>
+</td>
+<td width="125">
+<p>ESP32</p>
+</td>
+<td width="117">
+<p>STM32</p>
+<p>&nbsp;</p>
+</td>
+</tr>
+</tbody>
+</table>
+<ul>
+<li><strong><u>JavaScript Frameworks</u></strong><strong> :</strong></li>
+</ul>
+<p><strong><u>&nbsp;</u></strong></p>
+<table>
+<tbody>
+<tr>
+<td width="107">
+<p>Express JS</p>
+<p>&nbsp;</p>
+</td>
+<td width="109">
+<p>Socket.io</p>
+</td>
+<td width="103">
+<p><strong>Three JS</strong></p>
+</td>
+<td width="107">
+<p>Discord JS</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</td>
+<td width="92">
+<p>&nbsp;</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;The things i am familiar with</p>
+<table width="0">
+<tbody>
+<tr>
+<td width="154">
+<p>Figma</p>
+<p>&nbsp;</p>
+</td>
+<td width="141">
+<p>VLSI</p>
+</td>
+<td width="176">
+<p>Networking</p>
+</td>
+<td width="124">
+<p>Heroku</p>
+</td>
+</tr>
+<tr>
+<td width="154">
+<p>Php</p>
+</td>
+<td width="141">
+<p>Python</p>
+</td>
+<td width="176">
+<p>Web sockets Programming</p>
+<p>&nbsp;</p>
+</td>
+<td width="124">
+<p>Oops</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p><strong><u>PROJECTS</u></strong><strong><u>:</u></strong></p>
+<table width="0">
+<tbody>
+<tr>
+<td width="205">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://github.com/PSR0001/Online-store">Online-store</a></p>
+</td>
+<td width="219">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://playing-with-words.netlify.app/">Playing-with-words</a></p>
+</td>
+<td width="181">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://github.com/PSR0001/cleanurge-mcu">Cleanurge-mcu</a></p>
+</td>
+</tr>
+<tr>
+<td width="205">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://github.com/PSR0001/Path_coverage_ros">Path-coverage-ros</a></p>
+</td>
+<td width="219">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://psr0001.github.io/Note-App/">Note-App</a></p>
+</td>
+<td width="181">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://github.com/PSR0001/IoT-RGB-Socket">IoT-RGB-Socket</a></p>
+</td>
+</tr>
+<tr>
+<td width="205">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://github.com/PSR0001/discord_bot">Discord-Bot</a></p>
+</td>
+<td width="219">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://psr0001.github.io/PostMan-Clone/">Post-Me</a></p>
+</td>
+<td width="181">
+<p>&middot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://github.com/PSR0001/IoT-Distance-Socket">IoT-Distance-Socket</a></p>
+</td>
+</tr>
+</tbody>
+</table>
+<p><strong><u>TRAININGS/ COURSES</u></strong><strong>:</strong></p>
+<ul>
+<li>Introduction to Cybersecurity (CISCO)</li>
+<li>VLSI SoC Design using Verilog HDL(Maven Silicon)</li>
+<li>JavaScript (<a href="https://www.hackerrank.com/certificates/2f918bafe19e">Hackerrank</a>)</li>
+</ul>
+<p><strong><u>&nbsp;</u></strong></p>
+<p><strong><u>PORTFOLIO/ WORK SAMPLES</u></strong><strong>:&nbsp; </strong>My Portfolio website: <a href="https://coold69.netlify.app/">https://coold69.netlify.app/</a></p>
+<table>
+<tbody>
+<tr>
+<td width="124">
+<p>&Oslash;&nbsp; <a href="https://github.com/PSR0001">Github</a></p>
+</td>
+<td width="124">
+<p>&Oslash;&nbsp; <a href="https://www.linkedin.com/in/partha-singha-roy-33b983201/">Linkdin</a></p>
+<p>&nbsp;</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p><strong><u>ADDITIONAL DETAILS</u></strong><strong>:</strong></p>
+<ul>
+<li>Finalist in "<strong>AIITRA Robotics Challenge 2021</strong>" (Top 10 in 200+ teams)(<strong>Hackathon</strong>).</li>
+<li><strong>Domain Head</strong> of <strong>Core Electronics &amp; IoT</strong> in KGEC Robotics Society.</li>
+<li><strong>Core team </strong>member of <strong>GDSC</strong> (Google Developer Student Club) <strong>IoT Domain</strong>.</li>
+</ul>
+    </div>
+  )
+}
+
+export default Cv
